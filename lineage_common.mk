SigmaDroid-Devices/device_google_gs101

gs101: add service to disable HbmSVManager during boot process.
* We can't out-right delete the package because Pixel Thermometer for some reason relies on its presence for sensor permissions. So we just uninstall it for the user but keep the data so it can be used by Thermometer.

With this, we can switch to EvoX's PixelParts for HBM, and enjoy the extra battery savings.

Change-Id: I683cb36b64e91d5deeaf3f700c09cda86c3b1c8b
Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

lineage_common.mk
@@ -25,6 +25,10 @@ PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# HbmSVManager
+PRODUCT_COPY_FILES += \
+    device/google/gs101/conf/disable_hbmsv.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/disable_hbmsv.sh
+
 ifneq ($(BOARD_WITHOUT_RADIO),true)
 # product permissions XML from stock
 PRODUCT_COPY_FILES += \

SigmaDroid-Devices/device_google_gs201

lineage_common.mk
@@ -26,6 +26,10 @@ PRODUCT_COPY_FILES += \
     device/google/gs201/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
 endif
 
+# HbmSVManager
+PRODUCT_COPY_FILES += \
+    device/google/gs201/conf/disable_hbmsv.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/disable_hbmsv.sh
+
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 